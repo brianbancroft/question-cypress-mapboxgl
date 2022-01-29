9279b34f6e0c7fa16fee20f7476c97d5cc310c84
Brian Bancroft <hello@brianbancroft.ca>
2022-01-28 16:16:32 -0800

    Create map

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+import Map from "./Map";
+
+function App() {
+  return (
+    <main>
+      <header>simple map in react</header>
+      <div className="App">
+        <Map />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Map.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useState } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const styles = {
+  width: "100vw",
+  height: "calc(100vh - 80px)",
+  position: "absolute",
+};
+
+const MapboxGLMap = () => {
+  const [map, setMap] = useState(null);
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_GL_KEY;
+    const initializeMap = ({ setMap, mapContainer }) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+        center: [0, 0],
+        zoom: 5,
+      });
+
+      map.on("load", () => {
+        setMap(map);
+        map.resize();
+      });
+    };
+
+    if (!map) initializeMap({ setMap, mapContainer });
+  }, [map]);
+
+  return <div ref={(el) => (mapContainer.current = el)} style={styles} />;
+};
+
+export default MapboxGLMap;
