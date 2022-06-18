b5ed33167ed00f29314732b849445dd4a3fe97ee
Brian Bancroft <hello@brianbancroft.ca>
2022-06-18 16:49:44 -0700

    More changes

--- cypress/integration/2-workingMapActions.spec.js
+++ cypress/integration/2-workingMapActions.spec.js
@@ -1,3 +1,71 @@
+const mapCanvas = doucment.querySelector("canvas");
+const context = mapCanvas.getContext("webgl");
+
+let event = new MouseEvent("click", {
+  clientX: 500,
+  clientY: 500,
+  bubbles: true,
+});
+
+const mouseDown = (x, y) => {
+  document.querySelector("canvas").dispatchEvent(
+    new MouseEvent("click", {
+      clientX: x,
+      clientY: y,
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+};
+
+const mouseMove = (x, y, dx, dy) => {
+  document.querySelector("canvas").dispatchEvent(
+    new MouseEvent("mousemove", {
+      clientX: x + dx,
+      clientY: y + dy,
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+};
+
+const mouseUp = (x, y, dx, dy) => {
+  document.querySelector("canvas").dispatchEvent(
+    new MouseEvent("mouseup", {
+      clientX: x + dx,
+      clientY: y + dy,
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+};
+
+const dragEvent = new DragEvent("dragstart", {
+  clientY: 300,
+  clientX: 300,
+  bubbles: true,
+});
+
+const drag = new DragEvent("drag", {
+  clientY: 400,
+  clientX: 400,
+  bubbles: true,
+});
+
+const dragEnd = new DragEvent("dragend", {
+  clientY: 400,
+  clientX: 400,
+  bubbles: true,
+});
+
+function drag(x, y, dx, dy) {
+  mouseDown(x, y);
+  mouseMove(x, y, dx, dy);
+  mouseUp(x, y, dx, dy);
+}
+
+drag(400, 400, 200, 200);
+
 describe("Working map actions", () => {
   beforeEach(() => {
     cy.visit("https://cypress-mapboxgl-click-event.netlify.app/");
